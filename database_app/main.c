81252c5f3df06d604e6beb12148a646c16d05abd
tplaymeow <wersa2002@gmail.com>
2023-09-01 22:59:43 +0300

    init project

---
+++ database_app/main.c
@@ -0,0 +1,7 @@
+#include "database_library.h"
+
+int main(int argc, char **argv) {
+  const char *name = argv[1];
+  say_hello(name);
+  return 0;
+}
---
+++ database_library/database_library.c
@@ -0,0 +1,7 @@
+#include "database_library.h"
+
+#include <stdio.h>
+
+void say_hello(const char *name) {
+  printf("Hello, %s!\n", name);
+}
