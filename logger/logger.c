3644c18d07798480b54aded1dcc0eaa37b96fc65
tplaymeow <tplaymeow@users.noreply.github.com>
2023-09-04 12:43:17 +0000

    [CI] format files

--- logger/logger.c
+++ logger/logger.c
@@ -6,9 +6,7 @@
 static const char *logger_level_text[] = {
     [LOGGER_DEBUG] = "DEBUG", [LOGGER_INFO] = "INFO"};
 
-static void printf_varargs(const char *format, ...) {
-
-}
+static void printf_varargs(const char *format, ...) {}
 void logger_log(enum logger_level level, const char *file, int line,
                 const char *format, ...) {
   const char *level_text = logger_level_text[level];
