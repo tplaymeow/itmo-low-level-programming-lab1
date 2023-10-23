47d00e9e8a98217fb4d6e18b8f4040366994928e
tplaymeow <tplaymeow@users.noreply.github.com>
2023-10-23 00:10:02 +0000

    [CI] format files

--- database/database.c
+++ database/database.c
@@ -10,7 +10,9 @@ struct database {};
 
 struct database_table {};
 
-struct database *database_create() { return malloc(sizeof(struct database)); }
+struct database *database_create() {
+  return malloc(sizeof(struct database));
+}
 
 void database_destroy(struct database *database) {
   if (database) {
