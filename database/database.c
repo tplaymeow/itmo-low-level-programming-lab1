64c7ec888e464ff245587a5fba574e5781d3d262
tplaymeow <wersa2002@gmail.com>
2023-10-23 03:19:54 +0300

    try to fix windows build

--- database/database.c
+++ database/database.c
@@ -6,22 +6,20 @@
 
 #include <stdlib.h>
 
-struct database {};
-
-struct database_table {};
-
-struct database *database_create() {
-  return malloc(sizeof(struct database));
-}
-
-void database_destroy(struct database *database) {
-  if (database) {
-    free(database);
-  }
-}
-
-struct database_table *
-database_create_table(struct database *database,
-                      struct database_create_table_request request) {
-  return malloc(sizeof(struct database_table));
-}
+//struct database {};
+//
+//struct database_table {};
+//
+//struct database *database_create() { return malloc(sizeof(struct database)); }
+//
+//void database_destroy(struct database *database) {
+//  if (database) {
+//    free(database);
+//  }
+//}
+//
+//struct database_table *
+//database_create_table(struct database *database,
+//                      struct database_create_table_request request) {
+//  return malloc(sizeof(struct database_table));
+//}
--- database/database.h
+++ database/database.h
@@ -7,16 +7,16 @@
 
 #include "database_create_table_request.h"
 
-struct database;
-
-struct database_table;
-
-struct database *database_create();
-
-void database_destroy(struct database *database);
-
-struct database_table *
-database_create_table(struct database *database,
-                      struct database_create_table_request request);
+//struct database;
+//
+//struct database_table;
+//
+//struct database *database_create();
+//
+//void database_destroy(struct database *database);
+//
+//struct database_table *
+//database_create_table(struct database *database,
+//                      struct database_create_table_request request);
 
 #endif // ITMO_LOW_LEVEL_PROGRAMMING_LAB1_DATABASE_H
