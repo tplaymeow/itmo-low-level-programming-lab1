157d01fe27447403acf68d6479b6031c9eb9757f
tplaymeow <wersa2002@gmail.com>
2023-10-29 16:04:49 +0300

    test new workflow

--- database_test_app/main.c
+++ database_test_app/main.c
@@ -2,9 +2,12 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "database.h"
 #include "logger.h"
+
+enum mode { MODE_ONLY_INSERTS, MODE_ONLY_SELECTS, MODE_ALL };
 
 void print_all_rows(struct database *database, struct database_table table,
                     struct database_where where) {
@@ -192,57 +195,82 @@ void insert_post(struct database *database, struct database_table table,
 }
 
 int test1(int argc, char **argv) {
-  FILE *file = fopen(argv[1], "rb+");
+  enum mode mode;
+  if (strcmp(argv[1], "MODE_ONLY_INSERTS") == 0) {
+    mode = MODE_ONLY_INSERTS;
+  } else if (strcmp(argv[1], "MODE_ONLY_SELECTS") == 0) {
+    mode = MODE_ONLY_SELECTS;
+  } else {
+    mode = MODE_ALL;
+  }
+
+  FILE *file = fopen(argv[2], "rb+");
   if (file == NULL) {
     debug("File open failed");
     return EXIT_FAILURE;
   }
 
-  struct database *database = database_create_and_init(file);
+  struct database *database = mode == MODE_ONLY_SELECTS
+                                  ? database_init(file)
+                                  : database_create_and_init(file);
   if (database == NULL) {
     debug("DB init failed");
     return EXIT_FAILURE;
   }
 
-  struct database_table users_table = create_users_table(database);
+  struct database_table users_table =
+      mode == MODE_ONLY_SELECTS
+          ? database_get_table_with_name(database, "Users").table
+          : create_users_table(database);
 
-  insert_user(database, users_table, 0, "Timur0", "12345", 1.5);
-  insert_user(database, users_table, 1, "Timur1", "12345", 2.5);
-  insert_user(database, users_table, 2, "Timur2", "12345", 3.5);
-  insert_user(database, users_table, 3, "Timur3", "12345", 4.5);
-  insert_user(database, users_table, 4, "Timur4", "12345", 1.5);
-  insert_user(database, users_table, 5, "Timur5", "12345", 2.5);
-  insert_user(database, users_table, 6, "Timur6", "12345", 3.5);
-  insert_user(database, users_table, 7, "Timur7", "12345", 4.5);
+  if (mode != MODE_ONLY_SELECTS) {
+    insert_user(database, users_table, 0, "Timur0", "12345", 1.5);
+    insert_user(database, users_table, 1, "Timur1", "12345", 2.5);
+    insert_user(database, users_table, 2, "Timur2", "12345", 3.5);
+    insert_user(database, users_table, 3, "Timur3", "12345", 4.5);
+    insert_user(database, users_table, 4, "Timur4", "12345", 1.5);
+    insert_user(database, users_table, 5, "Timur5", "12345", 2.5);
+    insert_user(database, users_table, 6, "Timur6", "12345", 3.5);
+    insert_user(database, users_table, 7, "Timur7", "12345", 4.5);
+  }
 
-  printf("\nSelecting all rows\n");
-  print_all_rows(database, users_table, DATABASE_WHERE_ALWAYS);
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all rows\n");
+    print_all_rows(database, users_table, DATABASE_WHERE_ALWAYS);
+  }
 
-  printf("\nSelecting all rows with id > 4\n");
-  print_all_rows(database, users_table,
-                 (struct database_where){
-                     .attribute_position = 0,
-                     .operation = DATABASE_WHERE_OPERATION_GREATER,
-                     .value = (union database_attribute_value){.integer = 4}});
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all rows with id > 4\n");
+    print_all_rows(
+        database, users_table,
+        (struct database_where){
+            .attribute_position = 0,
+            .operation = DATABASE_WHERE_OPERATION_GREATER,
+            .value = (union database_attribute_value){.integer = 4}});
+  }
 
-  printf("\nSelecting all rows with username = Timur3\n");
-  print_all_rows(
-      database, users_table,
-      (struct database_where){
-          .attribute_position = 1,
-          .operation = DATABASE_WHERE_OPERATION_EQUAL,
-          .value = (union database_attribute_value){.string = "Timur3"}});
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all rows with username = Timur3\n");
+    print_all_rows(
+        database, users_table,
+        (struct database_where){
+            .attribute_position = 1,
+            .operation = DATABASE_WHERE_OPERATION_EQUAL,
+            .value = (union database_attribute_value){.string = "Timur3"}});
+  }
 
-  printf("\nSelecting all rows with username = Timur\n");
-  print_all_rows(
-      database, users_table,
-      (struct database_where){
-          .attribute_position = 1,
-          .operation = DATABASE_WHERE_OPERATION_EQUAL,
-          .value = (union database_attribute_value){.string = "Timur"}});
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all rows with username = Timur\n");
+    print_all_rows(
+        database, users_table,
+        (struct database_where){
+            .attribute_position = 1,
+            .operation = DATABASE_WHERE_OPERATION_EQUAL,
+            .value = (union database_attribute_value){.string = "Timur"}});
+  }
 
-  printf("\nRemoving row with username = Timur3\n");
-  {
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nRemoving row with username = Timur3\n");
     struct database_select_row_result select_result = database_select_row_first(
         database, users_table,
         (struct database_where){
@@ -252,8 +280,8 @@ int test1(int argc, char **argv) {
     database_remove_row(database, select_result.row);
   }
 
-  printf("\nRemoving row with id = 6\n");
-  {
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nRemoving row with id = 6\n");
     struct database_select_row_result select_result = database_select_row_first(
         database, users_table,
         (struct database_where){
@@ -263,37 +291,48 @@ int test1(int argc, char **argv) {
     database_remove_row(database, select_result.row);
   }
 
-  printf("\nSelecting all rows\n");
-  print_all_rows(database, users_table, DATABASE_WHERE_ALWAYS);
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all rows\n");
+    print_all_rows(database, users_table, DATABASE_WHERE_ALWAYS);
+  }
 
-  const struct database_table posts_table = create_posts_table(database);
+  struct database_table posts_table =
+      mode == MODE_ONLY_SELECTS
+          ? database_get_table_with_name(database, "Posts").table
+          : create_posts_table(database);
 
-  insert_post(database, posts_table, 0, 1001, "Title 1", "Message 1", 0);
-  insert_post(database, posts_table, 1, 1002, "Title 2", "Message 2", 1);
-  insert_post(database, posts_table, 2, 1003, "Title 3", "Message 3", 2);
-  insert_post(database, posts_table, 3, 1004, "Title 4", "Message 4", 4);
-  insert_post(database, posts_table, 4, 1005, "Title 5", "Message 5", 5);
-  insert_post(database, posts_table, 5, 1006, "Title 6", "Message 6", 7);
-  insert_post(database, posts_table, 6, 1007, "Title 7", "Message 7", 1);
-  insert_post(database, posts_table, 7, 1008, "Title 8", "Message 8", 1);
-  insert_post(database, posts_table, 8, 1009, "Title 9", "Message 9", 1);
-  insert_post(database, posts_table, 9, 1010, "Title 10", "Message 10", 7);
-  insert_post(database, posts_table, 10, 1011, "Title 11", "Message 11", 4);
-  insert_post(database, posts_table, 11, 1012, "Title 12", "Message 12", 5);
-  insert_post(database, posts_table, 12, 1013, "Title 13", "Message 13", 5);
-  insert_post(database, posts_table, 13, 1014, "Title 14", "Message 14", 5);
-  insert_post(database, posts_table, 14, 1015, "Title 15", "Message 15", 0);
-  insert_post(database, posts_table, 15, 1016, "Title 16", "Message 16", 0);
-  insert_post(database, posts_table, 16, 1017, "Title 17", "Message 17", 7);
+  if (mode != MODE_ONLY_SELECTS) {
+    insert_post(database, posts_table, 0, 1001, "Title 1", "Message 1", 0);
+    insert_post(database, posts_table, 1, 1002, "Title 2", "Message 2", 1);
+    insert_post(database, posts_table, 2, 1003, "Title 3", "Message 3", 2);
+    insert_post(database, posts_table, 3, 1004, "Title 4", "Message 4", 4);
+    insert_post(database, posts_table, 4, 1005, "Title 5", "Message 5", 5);
+    insert_post(database, posts_table, 5, 1006, "Title 6", "Message 6", 7);
+    insert_post(database, posts_table, 6, 1007, "Title 7", "Message 7", 1);
+    insert_post(database, posts_table, 7, 1008, "Title 8", "Message 8", 1);
+    insert_post(database, posts_table, 8, 1009, "Title 9", "Message 9", 1);
+    insert_post(database, posts_table, 9, 1010, "Title 10", "Message 10", 7);
+    insert_post(database, posts_table, 10, 1011, "Title 11", "Message 11", 4);
+    insert_post(database, posts_table, 11, 1012, "Title 12", "Message 12", 5);
+    insert_post(database, posts_table, 12, 1013, "Title 13", "Message 13", 5);
+    insert_post(database, posts_table, 13, 1014, "Title 14", "Message 14", 5);
+    insert_post(database, posts_table, 14, 1015, "Title 15", "Message 15", 0);
+    insert_post(database, posts_table, 15, 1016, "Title 16", "Message 16", 0);
+    insert_post(database, posts_table, 16, 1017, "Title 17", "Message 17", 7);
+  }
 
-  printf("\nSelecting all rows\n");
-  print_all_rows(database, posts_table, DATABASE_WHERE_ALWAYS);
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all rows\n");
+    print_all_rows(database, posts_table, DATABASE_WHERE_ALWAYS);
+  }
 
-  printf("\nSelecting all users joined with posts\n");
-  print_all_joined(database, users_table, posts_table, DATABASE_WHERE_ALWAYS,
-                   DATABASE_WHERE_ALWAYS,
-                   (struct database_join){.left_attribute_position = 0,
-                                          .right_attribute_position = 4});
+  if (mode != MODE_ONLY_INSERTS) {
+    printf("\nSelecting all users joined with posts\n");
+    print_all_joined(database, users_table, posts_table, DATABASE_WHERE_ALWAYS,
+                     DATABASE_WHERE_ALWAYS,
+                     (struct database_join){.left_attribute_position = 0,
+                                            .right_attribute_position = 4});
+  }
 
   database_table_destroy(users_table);
   database_table_destroy(posts_table);
